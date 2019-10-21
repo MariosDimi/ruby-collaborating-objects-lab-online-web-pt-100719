7118c51c44e73f915b7d42938940dc9c883f2af9
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 00:56:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
-class Song 
-  
-  attr_accessor :name, :artist 
-  
+class Song
+
+  attr_accessor :name, :artist
+
   def initialize(name)
     @name = name
   end
-  
+
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
@@ -13,9 +13,10 @@ class Song
     song.artist_name = artist
     song
   end
-  
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
   end
+
 end
