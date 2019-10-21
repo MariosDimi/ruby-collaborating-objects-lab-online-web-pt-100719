0a81836d865cb56bb2aa8f93f763637569f5df79
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 00:56:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,45 +1,51 @@
-class Artist 
-  attr_accessor :name, :song 
-  @@all - []
-  
-  def initialize(name)
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = [] 
+
+  def initialize(name)#artist_name
     @name = name
-    @songs = []
+    @songs = [] #array of songs for each instance
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def add_song(song)
-    @song << song
+    @songs << song
   end
-  
+
   def self.find_or_create_by_name(name)
     if self.find(name)
       self.find(name)
     else
-      self.find(name)
+      self.create(name)
     end
   end
-  
+
+
   def self.find(name)
     @@all.find do |artist|
       artist.name == name
     end
   end
-  
+
   def self.create(name)
     artist = self.new(name)
     @@all << artist
     artist
   end
-  
+
+
+
   def save
     @@all << self
   end
-  
+
   def print_songs()
     puts @songs.collect {|x| x.name}
   end
+
 end
